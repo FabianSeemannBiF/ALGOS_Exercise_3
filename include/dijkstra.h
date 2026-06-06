b1765a1b3993f31195166dc164a19e587539b53b
FabianSeemannBiF <if25b223@technikum-wien.at>
2026-06-06 12:39:12 +0200

    implemented dijstra header file

--- include/dijkstra.h
+++ include/dijkstra.h
@@ -4,5 +4,8 @@
 
 #ifndef PROGRAMMIERBEISPIEL_3_DIJKSTRA_H
 #define PROGRAMMIERBEISPIEL_3_DIJKSTRA_H
+#include "graph_parser.h"
+
+void find_and_print_shortest_path(Graph &graph, const std::string& start, const std::string& end);
 
 #endif //PROGRAMMIERBEISPIEL_3_DIJKSTRA_H
