b04cf04d605441d92555b842382eddfb0d02c843
FabianSeemannBiF <if25b223@technikum-wien.at>
2026-06-06 12:38:06 +0200

    implemented graph_parser header file

--- include/graph_parser.h
+++ include/graph_parser.h
@@ -4,5 +4,18 @@
 
 #ifndef PROGRAMMIERBEISPIEL_3_GRAPH_PARSER_H
 #define PROGRAMMIERBEISPIEL_3_GRAPH_PARSER_H
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+struct Edge {
+    std::string to_station;
+    std::string line;
+    int cost;
+};
+
+using Graph = std::unordered_map<std::string, std::vector<Edge>>;
+
+Graph parse_graph_file(std::string& filename);
 
 #endif //PROGRAMMIERBEISPIEL_3_GRAPH_PARSER_H
